a24a04dd94f66d90a157fded4ce0c76596325708
NuReach <nureach46@gmail.com>
2024-06-28 02:33:50 +0700

    export

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,9 @@ function App() {
   const onAddText = () => {
     editor?.addText("hello")
   }
+  const onSetColor = () => {
+    editor?.setFillColor("#8ecae6")
+  }
   const addImage = (event)=>{
     const file = event.target.files[0];
 
@@ -40,23 +43,28 @@ function App() {
     };
     reader.readAsDataURL(file);
   };
+
+  const exportToPNG = () => {
+    const canvasData = editor?.canvas.toDataURL({ format: 'png' }); // Get canvas data as PNG
+    const link = document.createElement('a');
+    link.href = canvasData;
+    link.download = 'my_design.png'; // Set desired filename
+    link.click();
+  };
   
   useEffect(() => {
-
+    fabric.Image.fromURL('/vite.svg', (oImg) => {
+      oImg.selectable = false;
+      oImg.width =editor?.canvas.width;
+      oImg.height =editor?.canvas.height;
+      oImg.sendToBack();
+      editor?.canvas.add(oImg);
+      editor?.canvas.sendToBack(oImg);
+      editor?.canvas.renderAll(); 
+    });
   }, [fabric, editor])
 
-  const setBackgroundImage = (canvas) => {
-    console.log(canvas);
-    fabric.Image.fromURL('https://st2.depositphotos.com/1035837/8479/i/450/depositphotos_84798240-stock-photo-galaxy-stars-abstract-space-background.jpg', (oImg) => {
-      oImg.selectable = false;
-      oImg.width =canvas.width;
-      oImg.height =canvas.height;
-      oImg.sendToBack();
-      canvas.add(oImg);
-      canvas.sendToBack(oImg);
-      canvas.renderAll(); 
-    });
-  };
+
   return (
     <div>
       <button onClick={onAddCircle}>Add circle</button>
@@ -64,8 +72,13 @@ function App() {
       <button onClick={onAddText}> Add Text</button>
       <button onClick={onDelete}> Delete</button>
       <button onClick={onDeleteAll}> DeleteAll</button>
+      <button onClick={onSetColor}> Color</button>
       <input type="file" accept="image/*" onChange={addImage} />
-      <FabricJSCanvas className="sample-canvas" onReady={setBackgroundImage} />
+      <button onClick={exportToPNG}>Export as PNG</button>
+      <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+      <FabricJSCanvas className="sample-canvas" onReady={onReady} />
     </div>
   )
 }
